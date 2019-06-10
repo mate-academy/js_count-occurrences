801c29e37b9b7f7992bc3f8aa578346c85640e2b
artem-meleshko <timonlondon@hotmail.com>
2019-06-10 20:22:21 +0300

    Solution

--- src/countOccurrences.js
+++ src/countOccurrences.js
@@ -18,7 +18,14 @@
  *
  */
 function countOccurrences(phrase, letter) {
-  // write code here
+  let count = 0;
+
+  for (let i = 0; i < phrase.length; i++) {
+    if (phrase[i].toLowerCase() === letter) {
+      count++;
+    }
+  }
+  return count;
 }
 
 module.exports = countOccurrences;
