97e479f66a4774dc41a1c3acc0d654b38e67d88e
vladyslav223 <vladyslav223@gmail.com>
2019-08-13 17:08:53 +0300

    letter

--- src/countOccurrences.js
+++ src/countOccurrences.js
@@ -19,6 +19,10 @@
  */
 function countOccurrences(phrase, letter) {
   // write code here
+  let count = 0;
+  for (let i = 0; i <= phrase.length; i++) {
+    if (phrase[i] === letter) { count++; }
+  } return count;
 }
 
 module.exports = countOccurrences;
