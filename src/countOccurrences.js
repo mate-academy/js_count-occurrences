2fc701b40257c55760d2518ee5026e07fb951700
igaryok <igor.sobko@gmail.com>
2019-08-13 11:46:01 +0300

    solution

--- src/countOccurrences.js
+++ src/countOccurrences.js
@@ -18,7 +18,23 @@
  *
  */
 function countOccurrences(phrase, letter) {
-  // write code here
+  let countLetter = 0;
+  let startPossition = 0;
+  let foundPossition;
+
+  while (true) {
+    foundPossition
+    = phrase.toLowerCase().indexOf(letter.toLowerCase(), startPossition);
+
+    if (foundPossition === -1) {
+      break;
+    }
+
+    countLetter++;
+    startPossition = foundPossition + 1;
+  }
+
+  return countLetter;
 }
 
 module.exports = countOccurrences;
