7a04df6f928fe223d37185a734dde000cf65448f
olesyamogil <olesya.mogil@gmail.com>
2019-09-24 19:35:36 +0300

    Update src/countOccurrences.js
    Co-Authored-By: Остап П'ятковський <44294945+gestap@users.noreply.github.com>

--- src/countOccurrences.js
+++ src/countOccurrences.js
@@ -19,6 +19,7 @@
  */
 function countOccurrences(phrase, letter) {
   let count = 0;
+
   for (let i = 0; i < phrase.length; i++) {
     if (phrase[i].toUpperCase() === letter.toUpperCase()) {
       count++;
