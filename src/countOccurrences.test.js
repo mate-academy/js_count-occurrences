c78562d71fadfc6408c291e1668e68610c95edfb
mgrinko <glukym@gmail.com>
2020-05-05 14:58:34 +0300

    Tests were improved

--- src/countOccurrences.test.js
+++ src/countOccurrences.test.js
@@ -2,22 +2,32 @@
 
 const countOccurrences = require('./countOccurrences');
 
-test('Empty string', () => {
+test('Should find a single small letter', () => {
+  expect(countOccurrences('samsung', 'a'))
+    .toBe(1);
+});
+
+test('Should find multiple small letters', () => {
+  expect(countOccurrences('samsung is gnusmas', 's'))
+    .toBe(5);
+});
+
+test('Should work for big and small letters in phrase', () => {
+  expect(countOccurrences('Samsung is gnusmas', 's'))
+    .toBe(5);
+});
+
+test('Should work for a big letter', () => {
+  expect(countOccurrences('Abrakadabra', 'A'))
+    .toBe(5);
+});
+
+test('Should return 0 for an empty string', () => {
   expect(countOccurrences('', 'a'))
     .toBe(0);
 });
 
-test('No occurrences: b in Samsung', () => {
+test('Should return 0 for missing letter', () => {
   expect(countOccurrences('Samsung', 'b'))
     .toBe(0);
-});
-
-test('s in Samsung', () => {
-  expect(countOccurrences('Samsung', 's'))
-    .toBe(2);
-});
-
-test('a in Abrakadabra', () => {
-  expect(countOccurrences('Abrakadabra', 'a'))
-    .toBe(5);
 });
