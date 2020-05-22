0639695d3f9fdaa28bdb5142b3e5edd5516e1d7d
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-05-22 08:30:30 +0300

    solution

--- src/countOccurrences.js
+++ src/countOccurrences.js
@@ -19,6 +19,17 @@
  */
 function countOccurrences(phrase, letter) {
   // write code here
+  const phras = phrase.toLowerCase();
+  const lette = letter.toLowerCase();
+  let count = 0;
+
+  for (let i = 0; i < phras.length; i++) {
+    if (phras[i] === lette) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 module.exports = countOccurrences;
